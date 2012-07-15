13ff000596e06dcc9d83246d9112035b7223077a
77web <info@77-web.com>
2012-07-15 18:15:23 +0900

    機能テストを修正

--- test/functional/pc_frontend/opActiveMemberPluginComponentsTest.php
+++ test/functional/pc_frontend/opActiveMemberPluginComponentsTest.php
@@ -2,7 +2,6 @@
 
 include dirname(__FILE__).'/../../bootstrap/functional.php';
 $browser = new opTestFunctional(new sfBrowser(), new lime_test(null, new lime_output_color()));
-$browser->setMobile();
 include dirname(__FILE__).'/../../bootstrap/database.php';
 
 $browser->setCulture('en');
